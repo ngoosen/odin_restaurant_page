c83d932167cceb3d4907e1db8f774ab4f74b3fdf
Noémie Goosen <noemie.goosen@gmail.com>
2024-11-28 13:54:44 +0100

    feat: separate tab pages into modules

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import "./styles.css";
 
+import Home from "./home";
+
 (function(doc) {
-  const titleDiv = doc.createElement("section");
-  titleDiv.classList.add("title");
-
-  const mainTitle = doc.createElement("h1");
-  mainTitle.innerText = "The best restaurant in town!";
-
-  titleDiv.appendChild(mainTitle);
-  doc.body.appendChild(titleDiv);
-
-  const textDiv = doc.createElement("section");
-
-  const textParagraph1 = doc.createElement("p");
-  textParagraph1.innerText = "Can you believe how awesome our restaurant is? Cause I can't! I mean look at those tacos, they are absolutely magnificent!";
-  const textParagraph2 = doc.createElement("p");
-  textParagraph2.innerText = "So watcha waiting for, pal? Come over at 25 Taco Street in TacoTown and have a try at our signature tacos!";
-
-  textDiv.appendChild(textParagraph1);
-  textDiv.appendChild(textParagraph2);
-  doc.body.appendChild(textDiv);
+  Home(doc);
 })(document);
