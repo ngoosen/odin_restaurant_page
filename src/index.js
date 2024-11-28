6d0d62418601f2f829d3a477b519d348c7cba913
Noémie Goosen <noemie.goosen@gmail.com>
2024-11-28 14:05:43 +0100

    feat: setup tab nav buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ import "./styles.css";
 
 import Home from "./home";
 
+function clear(doc) {
+  doc.getElementById("content").innerHTML = "";
+}
+
 (function(doc) {
   Home(doc);
+
+  const homeButton = doc.getElementById("home");
+  homeButton.addEventListener("click", () => {
+    clear(doc);
+    Home(doc);
+  });
+
+  const menuButton = doc.getElementById("menu");
+  menuButton.addEventListener("click", () => {
+    console.log("menu");
+  });
+
+  const aboutButton = doc.getElementById("about");
+  aboutButton.addEventListener("click", () => {
+    console.log("about");
+  });
+
+  const contactButton = doc.getElementById("contact");
+  contactButton.addEventListener("click", () => {
+    console.log("contact");
+  });
 })(document);
